f32a329cb69fc56807bdd1e0d0a0c28d6c068515
Sarah Allateef <sara.korea96@gmail.com>
2023-11-20 18:25:41 +0300

    Initial Commit

---
+++ swiftui-30day/swiftui_30dayApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_30dayApp.swift
+//  swiftui-30day
+//
+//  Created by  MACBOOK AIR on 20/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_30dayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
